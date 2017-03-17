873f3cd00ec3a6af5816677e181edda6d79813a3
MaxUlysse <max@ithake.eu>
2017-03-17 11:02:55 +0100

    corrected typo in test

--- tests/build_all_docker.sh
+++ tests/build_all_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+# snpeff is not included in the test, because the build takes too long...
 echo "Starting Nextflow... Command:"
-echo "nextflow run . --docker --containers bcftools,concatvcf,fastqc,gatk,multiqc,mutect1,picard,mapreads,runallelecount,runascat,runconvertallelecounts,runmanta,strelka,samtools"
+echo "./nextflow run . --docker --containers bcftools,concatvcf,fastqc,gatk,multiqc,mutect1,picard,mapreads,runallelecount,runascat,runconvertallelecounts,runmanta,samtools,strelka"
 echo "-----"
-nextflow run . --docker --containers bcftools,concatvcf,fastqc,gatk,multiqc,mutect1,picard,mapreads,runallelecount,runascat,runconvertallelecounts,runmanta,strelka,samtools
+./nextflow run . --docker --containers bcftools,concatvcf,fastqc,gatk,multiqc,mutect1,picard,mapreads,runallelecount,runascat,runconvertallelecounts,runmanta,samtools,strelka
