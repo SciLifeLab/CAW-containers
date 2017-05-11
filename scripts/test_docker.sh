ccc656e500787cf623cf9688761bae81b17a7cfd
MaxUlysse <max@ithake.eu>
2017-05-11 14:55:11 +0200

    remove vardict from script

--- scripts/test_docker.sh
+++ scripts/test_docker.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+set -xeuo pipefail
 
-# snpeff is not included in the test, because the build takes too long...
-echo "Starting Nextflow... Command:"
-echo "nextflow run . --docker --containers bcftools,concatvcf,fastqc,gatk,htslib,igvtools,mapreads,multiqc,mutect1,picard,qualimap,runallelecount,runascat,runconvertallelecounts,runmanta,samtools,strelka,vardictjava"
-echo "-----"
-nextflow run . --docker --containers bcftools,concatvcf,fastqc,gatk,htslib,igvtools,mapreads,multiqc,mutect1,picard,qualimap,runallelecount,runascat,runconvertallelecounts,runmanta,samtools,strelka,vardictjava
+# snpeff and vep are not included in the test, because the build takes too long...
+
+nextflow run . --docker --containers bcftools,concatvcf,fastqc,gatk,htslib,igvtools,mapreads,multiqc,mutect1,picard,qualimap,runallelecount,runascat,runconvertallelecounts,runmanta,samtools,strelka
