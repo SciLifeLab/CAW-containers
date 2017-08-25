2f5948bf01ddd42d97aa11f396ab407563d3144d
Maxime Garcia <max.u.garcia@gmail.com>
2017-08-25 14:29:34 +0200

    enhance build/push/pull script

--- scripts/build_and_push_all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -xeuo pipefail
-
-nextflow run . --docker --push --containers bcftools,concatvcf,fastqc,gatk,htslib,igvtools,multiqc,mutect1,picard,qualimap,runascat,runconvertallelecounts,samtools,strelka,snpeff,vep
-
-nextflow run . --docker --push --containers mapreads,runallelecount,runmanta,snpeffgrch37,snpeffgrch38,vepgrch37,vepgrch38
