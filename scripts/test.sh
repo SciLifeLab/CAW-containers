86c291719034e271a9b8de573a088f113d8fe2a2
Maxime Garcia <max.u.garcia@gmail.com>
2017-09-22 09:47:25 +0200

    remove concatvcf from test

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,4 +3,4 @@ set -xeuo pipefail
 
 # snpeffgrch37/snpeffgrch38 and vepgrch37/vepgrch38 are not included in the test, because the build takes too long...
 
-nextflow run . --docker --containers bcftools,concatvcf,fastqc,gatk,htslib,igvtools,mapreads,multiqc,mutect1,picard,qualimap,runallelecount,runascat,runconvertallelecounts,runmanta,samtools,strelka,snpeff,vep
+nextflow run . --docker --containers bcftools,fastqc,gatk,htslib,igvtools,mapreads,multiqc,mutect1,picard,qualimap,runallelecount,runascat,runconvertallelecounts,runmanta,samtools,strelka,snpeff,vep
