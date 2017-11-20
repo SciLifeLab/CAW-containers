c28b93fa92c5344c30d2e09af473ec0c29ab451a
Maxime Garcia <max.u.garcia@gmail.com>
2017-11-20 14:04:20 +0100

    archiving project

--- scripts/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -xeuo pipefail
-
-# snpeffgrch37/snpeffgrch38 and vepgrch37/vepgrch38 are not included in the test, because the build takes too long...
-
-nextflow run . --docker --containers bcftools,fastqc,gatk,htslib,igvtools,mapreads,multiqc,mutect1,picard,qualimap,runallelecount,runascat,runconvertallelecounts,runmanta,samtools,strelka,snpeff,vep
